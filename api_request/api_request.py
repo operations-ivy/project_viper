f1771c0e07dec2ba137711ed83ca84f5fe556914
Patrick White <whitep@vcu.edu>
2024-02-01 11:26:26 -0500

    adding random stuff to this random git repo

--- api_request/api_request.py
+++ api_request/api_request.py
@@ -1,23 +1,29 @@
+import time
 import json
 import requests
 
-class ApiRequest:
-    def get_random(self):
-        """
-        Returns a random Chuck Norris joke.
-        """
-        random_chuck_jokes = "https://api.chucknorris.io/jokes/random"
-        response = requests.get(random_chuck_jokes)
-        json_data = json.loads(response.text)
-        
-        return json_data
+# class ApiRequest:
+def get_random():
+    """
+    Returns a random Chuck Norris joke.
+    """
+    random_chuck_jokes = "https://api.chucknorris.io/jokes/random"
+    response = requests.get(random_chuck_jokes)
+    json_data = json.loads(response.text)
     
-    def find_specific(self, query):
-        """
-        Returns a list of Chuck Norris jokes that match the query.
-        """
-        query_chuck_jokes = "https://api.chucknorris.io/jokes/search?query=" + query
-        response = requests.get(query_chuck_jokes)
-        json_data = json.loads(response.text)
-        
-        return json_data
+    return json_data
+
+def find_specific(query):
+    """
+    Returns a list of Chuck Norris jokes that match the query.
+    """
+    query_chuck_jokes = "https://api.chucknorris.io/jokes/search?query=" + query
+    response = requests.get(query_chuck_jokes)
+    json_data = json.loads(response.text)
+    
+    return json_data
+
+while True:
+    new_request = get_random()
+    print(new_request)
+    time.sleep(10)

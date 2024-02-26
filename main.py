b8b7900ab4dec489daf77b25b4be829a6a1d5cab
Patrick White <whitep@vcu.edu>
2024-02-26 14:07:23 -0500

    adding working sqlite middleware

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import api_request.api_request as api
 import sqlite_storage.sqlite_storage as storage
 
-api = api.ApiRequest()
-storage = storage.SqliteStorage(db_path="/home/zaphod/code/project_viper/sqlite_storage/sqlite.db")
 
-
-print(api.get_random()["id"])
-print(api.get_random()["value"])
-
-storage.insert_joke(api.get_random()["id"], api.get_random()["value"])
+if __name__ == "__main__":
+    api = api.ApiRequest()
+    storage = storage.SqliteStorage(db_path="/home/zaphod/code/project_viper/sqlite_storage/sqlite.db")
+    # print(api.get_random()["id"])
+    # print(api.get_random()["value"])
+    for i in range(100):
+        joke_id = api.get_random()["id"]
+        joke_value = api.get_random()["value"]
+        if storage.check_for_duplicate(joke_id) == None:
+            storage.insert_joke(joke_id, joke_value)
+            print("Joke added: " + joke_value)
+        else:
+            print("Duplicate joke found")
+    
+    print(storage.read_all_jokes())
+    storage.close_connection()
--- sqlite_storage/sqlite_storage.py
+++ sqlite_storage/sqlite_storage.py
@@ -1,16 +1,33 @@
 import sqlite3
+from sqlite3 import Error
 
 class SqliteStorage:
     def __init__(self, db_path):
-        db_path = db_path
-        self.conn = sqlite3.connect(db_path)
-        self.cur = self.conn.cursor()
+        self.db_path = db_path
+        self.conn = sqlite3.connect(self.db_path)
+        self.cursor = self.conn.cursor()
 
+    def close_connection(self):
+        self.cursor.close()
+        self.conn.close()
 
-    def check_if_joke_exists(self, joke_id):
-        pass
+    # def check_if_joke_exists(self, joke_id):
+    #     pass
 
     def insert_joke(self, joke_id, joke_value):
-        sql = ''' INSERT INTO chuck(id, value)
-                  VALUES(?,?) '''
-        self.cur.execute(sql, (joke_id, joke_value))
+        sql = ''' INSERT OR IGNORE INTO chuck(id,value)
+                VALUES(?,?) '''
+        self.cursor.execute(sql, (joke_id, joke_value))
+        self.conn.commit()
+    
+    def check_for_duplicate(self, joke_id):
+        sql = ''' SELECT * FROM chuck WHERE id = ? '''
+        self.cursor.execute(sql, (joke_id,))
+        
+        return self.cursor.fetchone()
+    
+    def read_all_jokes(self):
+        self.cursor.execute('SELECT * FROM chuck')
+
+        return self.cursor.fetchall()
+    

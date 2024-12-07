5d19ac6024076c6cd5eeff8e306c94e2ce0aa2e1
Zaphod Beeblebrox <whitep@vcu.edu>
2024-12-07 14:29:43 -0500

    adding other repo into mono repo

---
+++ local_inventory/local_inventory.py
@@ -0,0 +1,32 @@
+from flask import *
+import nmap
+import pandas as pd
+app = Flask(__name__)
+
+@app.route("/tables")
+def show_tables():
+    nm = nmap.PortScanner()
+
+    bad_cols = ["command_line", "scaninfo", "scanstats"]
+    good_cols = ["ip_address", "hostname"]
+    host_dict = nm.scan('192.168.4.*', '22')
+
+    df = pd.DataFrame(host_dict)
+
+    df.drop("nmap", axis=1, inplace=True)
+    df.drop(bad_cols, axis=0, inplace=True)
+
+    inventory = pd.DataFrame(columns=good_cols)
+    inventory["ip_address"] = df.index
+    inventory.index.rename("index", inplace=True)
+
+    df.reset_index(drop=True, inplace=True)
+
+    for i, row in inventory.iterrows():
+        inventory.iloc[i, inventory.columns.get_loc("hostname")] = df.scan[i].hostname()
+
+    return render_template('view.html',tables=[inventory.to_html()], titles = ['na'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
